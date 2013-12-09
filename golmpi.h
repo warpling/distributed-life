46f74385388588fd559a4390c790357c90de855c
michaellady <michaellady@gmail.com>
2013-12-08 17:23:53 -0800

    whoops, forgot golmpi.h

---
+++ golmpi.h
@@ -0,0 +1,22 @@
+#include <float.h>
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <stdint.h>
+#include "mpi.h"
+
+#define BUF_SIZE 10000
+#define TOP 0
+#define BOT 1
+#define ROOT 0
+
+int rank;
+int nprocs;
+
+int8_t* getGameTile(char *filename);
+int32_t getDeclaredElementCount(char * filename);
+
